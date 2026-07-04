4a12ae7fdadfe8b2fc4072b190f0b05a9dd65003
parthjha110 <jha.parth11@gmail.com>
2026-07-04 22:17:56 +0530

    feat: add base LLM provider

--- app/service/llm.py
+++ app/service/llm.py
@@ -1,3 +1,5 @@
-class LLMService : 
-    def generate(self, message: str):
+from app.core.base_llm import BaseLLMProvider
+
+class LLMService(BaseLLMProvider) : 
+    def generate(self, message: str) -> str:
         return f"NTD-Core Recived : {message}"
